ba370bd770a358ae75d5d1237cbcee2df2d73636
Dheeraj Bhosale <dbhosale@rocketsoftware.com>
2024-10-19 10:45:00 +0530

    Day 28 challenge complete.
    Leaned about WebStorage (Especially LocalStorage) CRUD.

--- main.js
+++ main.js
@@ -1,41 +1,113 @@
 
-
-const personalitiesList = [ 'instructor', 'student', 'mentor' ];
-
-const delays = [12, 4, 8]
-
-// add children to <div class="page-head__ticker">
-const ticker = document.getElementById('page-head__ticker');
+const table = document.getElementById('leaderboard');
 
 
-personalitiesList.forEach((personality,index) => {
+document.getElementById('addPlayer').addEventListener('click', addPlayer);
+function addPlayer() {
+    const playerScore = document.getElementById('playerScore').value;
+    const country = document.getElementById('country').value;
+    const lastName = document.getElementById('lastName').value;
+    const firstName = document.getElementById('firstName').value;
 
-    const msg = document.createElement('p');
-    msg.classList.add(`msg`);
-    
+    if (!firstName || !lastName || !country || !playerScore) {
+        return alert('All fields are required');
+    }
 
-    const span = document.createElement('span');
-    span.textContent = personality;
-    span.style.animationDelay = `${delays[index]}s`;
+    const player = {
+        firstName,
+        lastName,
+        country,
+        playerScore
+    };
 
-    msg.appendChild(span);
+    const players = JSON.parse(localStorage.getItem('players')) || [];
 
-    ticker.appendChild(msg);
+    players.push(player);
 
+    reshuffle(players);
+
+    document.getElementById('playerScore').value = '';
+    document.getElementById('country').value = '';
+    document.getElementById('lastName').value = '';
+    document.getElementById('firstName').value = '';
+
+    // alert('Player added to leaderboard');
+}
+
+// add dom content loaded event listener for initial view
+document.addEventListener('DOMContentLoaded', updateLeaderboard);
+
+function updateLeaderboard() {
+    const players = JSON.parse(localStorage.getItem('players')) || [];
+
+
+    console.log('players', players);
+
+    // clear the table
+    table.innerHTML = '';
+
+    // Insert header row
+    const row = table.insertRow(0);
+    row.innerHTML = `
+        <tr>
+            <th>Name</th>
+            <th>Country</th>
+            <th>Score</th>
+            <th>Actions</th>
+        </tr>
+        `;
+    if (players.length === 0) {
+        return;
+    }
+    else {
+
+        // Insert data rows
+        players.forEach((player, index) => {
+
+            const row = table.insertRow(index + 1);
+            row.innerHTML = `
+                <td>${player.firstName} ${player.lastName}</td>
+                <td>${player.country}</td>
+                <td>${player.playerScore}</td>
+                <td>
+                    <div class="actions">
+                    <button onclick="deletePlayer(${index})">🗑️</button>
+                    <button onclick="updateScore(${index}, 5)">+5</button>
+                    <button onclick="updateScore(${index}, -5)">-5</button>
+                    </div>
+                </td>
+            `;
+        });
+    }
+
+}
+
+window.addEventListener('storage', function (e) {
+    console.log("localstorage is", JSON.stringify(e.storageArea));
 });
 
+window.deletePlayer = function (index) {
+    const players = JSON.parse(localStorage.getItem('players')) || [];
 
+    players.splice(index, 1);
 
+    reshuffle(players);
+}
 
-const proglang = ["python", "REact", "Html"]
+window.updateScore = function (index, value) {
+    const players = JSON.parse(localStorage.getItem('players')) || [];
 
-const proglangText = document.getElementById('techName');
+    players[index].playerScore = parseInt(players[index].playerScore) + value;
 
-let proglangIndex = 0;
+    reshuffle(players);
+    
+}
 
-// change the text content of the techName element every 2 seconds
+window.reshuffle = function (players) {
+    // sort players based on score
+    players.sort((a, b) => a.playerScore - b.playerScore);
 
-setInterval(() => {
-    proglangIndex = (proglangIndex + 1) % proglang.length;
-    proglangText.textContent = proglang[proglangIndex];
-}, 2000);
+    localStorage.setItem('players', JSON.stringify(players));
+
+    updateLeaderboard();
+}
